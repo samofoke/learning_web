66e26d3a9c1eeb381a2536fb0eef2fe1b4e14bb2
samofoke <samofoke@student.wethinkcode.co.za>
2019-10-11 08:41:36 +0200

    web7

--- php/ssap.php
+++ php/ssap.php
@@ -8,3 +8,20 @@ if ($argc > 1)
     print_r($arr);
 }
 ?>
+  $tab = implode(" ", $argv);
+  $tab = preg_replace('/\s+/', ' ', $tab);
+  $tab = trim($tab);
+  $tab = explode(" ", $tab);
+  $name = array_shift($tab);
+  sort($tab);
+foreach ($tab as $elem) {
+  echo $elem."\n";
+
+ : $argv[1] = trim($argv[1]);
+  $argv[1] = preg_replace('/\s+/', ' ', $argv[1]);
+  $tab = explode(" ", $argv[1]);
+  $first = array_shift($tab);
+  $tab = implode(" ", $tab);
+  if (!empty($tab))
+    echo $tab." ";
+  echo $first."\n";
