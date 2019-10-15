8985b72e8782d0e8bfbf783164e9dc8c58650066
samofoke <samofoke@student.wethinkcode.co.za>
2019-10-15 11:14:45 +0200

    web8

---
+++ Store/index.php
@@ -0,0 +1,31 @@
+<?PHP
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body class="phase1">
+    <div class="all">
+        <header>
+            <div class="main">
+                <ul>
+                    <li class="main2"><a href="#">home</a></li>
+                    <li class="main2"><a href="#">home</a></li>
+                    <li class="main2"><a href="#">home</a></li>
+                    <li class="main2"><a href="#">home</a></li>
+                    <li class="right"><a href="#about">About</a></li>
+                </ul>
+            </div>
+        </header>
+        <div class="move1">
+                <img class="pic" src="images/wp1864140.jpg" width="1500" height="800">
+        </div>
+        <div class="logo">
+            <img src="/images/pi4.png">
+        </div>
+    </div>
+</body>
+</html>
